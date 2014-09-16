a2599353598da0661eb64c00128414ece3d290f7
charlesgo0og <go0oglenmnm1987@gmail.com>
2014-09-16 11:11:04 +0800

    网页基本框架
    第一次提交

--- web/Home/Lib/Action/IndexAction.class.php
+++ web/Home/Lib/Action/IndexAction.class.php
@@ -5,9 +5,9 @@ require_once 'Home/Common/DDlog.class.php';
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
-        echo "aaaaa";
+        //echo "bbbb";
+        $this->assign("oo", "wangqi");
     	$this->display("Index:index");
-    	
 	}
   
 }
---
+++ web/Home/Runtime/Cache/7e4848229090366086cc7eeb3bfc0798.php
@@ -0,0 +1,279 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>肏女神-真实女神自拍分享社区（caonvshen.com）</title>
+<meta name="description" content="肏女神lunvshen.com-真实女神的自拍分享社区。汇集最新、最全、最HOT的美女照片，美女视频，女神视频，最新女神排行榜，实乃撸管必备神器！ ">
+<meta name="keywords" content="女神,美女,女神排行榜,宅男女神,宅男女神排行榜,美女图片,美女自拍,女神自拍,女神图片,美女排行榜,撸管,撸美女,撸女神,撸妹纸">
+<link rel="stylesheet" type="text/css" href="semantic/packaged/css/semantic.css" />
+<script src="semantic/packaged/javascript/jquery.js"></script>
+<script src="semantic/packaged/javascript/semantic.min.js"></script>
+<link rel="shortcut icon" href="http://lunvshen.com/favicon.ico" /> 
+</head>
+<style>
+#example{
+    padding: 0px;
+    margin: 0px;
+    font-family: 'Lucida Grande', Arial, 'Hiragino Kaku Gothic Pro', Meiryo, sans-serif;
+
+}
+html,body{
+    font-size: 15px;
+}
+.container{
+    width: 970px;
+    margin: 0px auto;
+}
+.maindiv{
+    background-image: url("/img/background.png");
+    width: 100%;
+}
+#nvshenmenu{
+    margin: 0px;
+    width: 100%;
+}
+a {
+color: #733;
+text-decoration: none;
+}
+a:hover {
+text-decoration: underline;
+}
+a.imga:hover{
+    text-decoration: none;
+}
+a img{ 
+                border:none;
+            }
+.go-to-next-page, .random-again {
+    clear: both;
+border: 1px solid #edcfc9;
+background: #faf1ef;
+padding: 8px;
+-webkit-border-radius: 6px;
+-moz-border-radius: 6px;
+-ms-border-radius: 6px;
+-o-border-radius: 6px;
+border-radius: 6px;
+text-align: center;
+}
+.go-to-next-page a, .random-again a {
+display: block;
+width: 90%;
+margin: 0 auto;
+}
+.caption {
+    text-align: center;
+    position: relative;
+    bottom:41px;
+    padding:6px 20px 6px 20px;
+    margin:0xp;
+    background:#000;
+    background:rgba(0,0,0,.3);
+    width:255px;
+    color:#fff;
+    text-shadow:none;
+    height:18px;
+    border-bottom-right-radius:8px;
+border-bottom-left-radius:8px;
+font-size: 12px;
+margin-bottom: -37px;
+}
+.imgvideo{
+    position: relative;
+    left:258px;
+    top:-35px;
+}
+</style>
+<body id="example" class="maindiv" >
+<div class="ui red inverted menu" id="nvshenmenu">
+    <div class="container">
+            <div class="title item" style="margin-right:40px;float:left">
+                <a href="http://lunvshen.com"><b>肏女神</b></a>
+            </div>     
+         <a class="item" href="http://lunvshen.com">首页</a>
+         <a class="item" href="http://lunvshen.com/pp">相册</a>
+         <a class="item" href="http://lunvshen.com/video">视频</a>
+         <a class="item" href="http://lunvshen.com/random">随机肏</a>
+         <a class="item" href="http://lunvshen.com/users">女神排行</a>
+         <a class="item" href="http://lunvshen.com/add">推荐女神</a>
+    </div>
+</div>
+
+<div class="maindiv" style="text-shadow: 0 1px 0 #fff;">
+    <div class="container" style="padding: 20px 0px 30px 0px;">
+        
+<?php
+for ($rows=0;$rows<8;$rows++) { ?>
+<div style='display:block;clear:both;'> 
+    <?php
+ for ($columns=0;$columns<3;$columns++) { ?>
+        <div class="ui" style="float:left;width:323px">
+                <a target="_blank" href="http://lunvshen.com/u?name=绮里嘉ula" class="imga">
+                <img class="ui avatar image" style="width:36px;height:36px;z-index:100" src="http://tp3.sinaimg.cn/3714607270/50/5700847258/0" alt="绮里嘉ula的主页，绮里嘉ula的自拍照与自拍视频">
+                </a><a target="_blank" href="http://lunvshen.com/u?name=绮里嘉ula">绮里嘉ula</a><br/>
+            <div class="imga meinvfirstimg thumbnails yoxview" style="margin-right:1.55em;width:295px;display:block;cursor:pointer">
+                <a class="imga boxer" target="_blank" href='http://lunvshen.com/29046.html' >
+                    <img style="width:300px;margin-right:0em;margin-top:15px;border-radius: 8px;"  class="rounded ui  left image " src="http://ww1.sinaimg.cn/bmiddle/dd6868a6jw1ek3zaa7z6ij20hs0qo76i.jpg" alt="绮里嘉ula的自拍照">  
+                    <p class="caption">6张女神照，点击浏览</p>  
+                </a>
+            </div>
+            <p style="font-size: 12px;color: #707070; margin-right:1.55em; margin-top:-20px">送清凉壁纸了，你们有用我的照片做屏保么[挤眼]喜欢的转走哦 </p>
+            <div style="height:28px">
+            </div>
+
+        </div>
+    <?php
+ } ?>
+</div>
+<?php
+}?>
+
+    </div>
+</div>
+                
+<p class='go-to-next-page'><a href='http://lunvshen.com/?p=2'>下一页</a></p>            
+<div style="width:100%;text-align: center;clear: both;">
+          <!--  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-5486123269162001"
+     data-ad-slot="1261871373"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+ -->
+ <!--
+<script> var dsaid=32314; var dwidth=728; var dheight=90; </script> <script type="text/javascript" src="http://unionjs.dianxin.com/showPic.js" name="showpic" charset="utf-8" ></script>
+ -->
+<script src="http://e.70e.com/cpc_img.asp?u=62610&m=3&n=&s_px=2" charset="gb2312"></script>
+</div>
+            <style type="text/css">
+.foota{
+    font-size: 13px;
+}
+.sl-fixed-right {
+right: 0 !important;
+left: auto;
+_right: auto;
+_left: expression(eval(document.documentElement.scrollLeft+document.documentElement.clientWidth-this.offsetWidth)-(parseInt(this.currentStyle.marginLeft,10)||0)-(parseInt(this.currentStyle.marginRight,10)||0));
+}
+.sl-fixed-bottom {
+bottom: 0;
+top: auto;
+_bottom: auto;
+_top: expression(eval(document.documentElement.scrollTop+document.documentElement.clientHeight-this.offsetHeight-(parseInt(this.currentStyle.marginTop,10)||0)-(parseInt(this.currentStyle.marginBottom,10)||0)));
+}
+.sl-fixed {
+position: fixed!important;
+z-index: 300;
+}
+.backtop a {
+display: block;
+cursor: pointer;
+position: absolute;
+z-index: 99;
+bottom: 10px;
+right: 20px;
+width: 34px;
+height: 60px;
+background: url('../img/rocket.png') no-repeat 0 0;
+box-shadow: none;
+}
+.backtop a:hover {
+background-image: url('../img/rocket-active.png');
+}
+.f-sina,.f-qq{
+float: right;
+width: 28px;
+height: 28px;
+background-image: url('../img/follow.png');
+background-repeat: no-repeat;
+margin: 0 2px 0 0;
+}
+.f-qq {
+background-position: -75px top;
+}
+</style>
+<div class="ui divider" style="margin:25px 0px 20px 0px;clear:both"></div>
+
+<div class="ui inverted" id="nvshenmenu">
+    <div class="container" style="height:60px;padding:0px 10px;clear:both">
+        <div style="width:70%;float:left">
+         <div class="ui divided horizontal list">
+            <div class="item"><a href="http://lunvshen.com/about" target="_blank" class="foota">关于我们</a></div>
+            <div class="item"><a href="http://lunvshen.com/advertise" target="_blank" class="foota">广告投放</a></div>
+            <div class="item"><a href="http://lunvshen.com/link" target="_blank" class="foota">友链交换</a></div>
+             <div class="item"><a href="http://lunvshen.com/declear" target="_blank" class="foota">免责申明</a></div>
+             <!--
+                         <div class="item"><a href="http://lunvshen.com/pp" target="_blank" class="foota">女神相册</a></div>
+                          <div class="item"><a href="http://lunvshen.com/video" target="_blank" class="foota">女神视频</a></div>
+            <div class="item"><a href="http://lunvshen.com/users" target="_blank" class="foota">女神排行</a></div>
+            <div class="item"><a href="http://lunvshen.com/add" target="_blank" class="foota">推荐女神</a></div>
+        -->
+
+        </div>
+        <br/><br/>
+
+        <div style="font-size: 13px;">
+            <span>友情链接:&nbsp;&nbsp;&nbsp;</span> <a style="margin-right:10px" target="_blank" href="http://youai123.com">有爱导航</a>
+            <a style="margin-right:10px" target="_blank" href="http://site.youai123.com">上什么</a>
+        </div>
+
+    </div>
+     <div style="width:30%;float:right;text-align:right">
+        <span style="font-weight:500">关注撸女神</span>
+        <br/>
+        <br/>
+                <a href="http://t.qq.com/lunvshen" class="f-qq" target="_blank" title="撸女神的腾讯微博"></a>
+        <a href="http://weibo.com/lunvshen" class="f-sina" target="_blank" title="撸女神的新浪微博"></a>
+
+     </div>
+    </div>
+</div>
+<div id="toolBackTop" class="backtop sl-fixed sl-fixed-bottom sl-fixed-right" style="display: none;">
+<a class="br-s bs-l" onclick="window.scrollTo(0,0);return false;"></a>
+</div>
+<!--
+<script type="text/javascript" src="http://unionjs.dianxin.com/dl.js" name="dl" data-said="32329" data-width="120" data-height="270" data-type="0" charset="utf-8" ></script>
+-->
+<script src="http://f.ku63.com/f.asp?u=62610&m=3&n=" charset="gb2312"></script>
+<div style="display:none;">
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_5906485'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "v1.cnzz.com/stat.php%3Fid%3D5906485%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+</div>
+<script type="text/javascript">
+
+$(document).ready(function () {
+        var bt = $('#toolBackTop');
+        var sw = $(document.body)[0].clientWidth;
+
+        var limitsw = (sw - 840) / 2 - 80;
+        if (limitsw > 0){
+                limitsw = parseInt(limitsw);
+                bt.css("right",limitsw);
+        }
+
+        $(window).scroll(function() {
+                var st = $(window).scrollTop();
+                if(st > 30){
+                        bt.show();
+                }else{
+                        bt.hide();
+                }
+        });
+})
+
+</script>
+            </div>
+    </div>
+    <script type="text/javascript">
+    $(document).ready(function($) {
+        
+    });
+    </script>
+
+
+</body>
+</html>
