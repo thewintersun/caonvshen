1c4eeff8b9c0f6520a533aef1d2833a1fd888a42
thewintersun <thewintersun@thewintersuntekiMacBook-Pro.local>
2014-10-15 22:28:02 +0800

    移动广告，提高点击绿

--- web/Public/js/yxj.js
+++ web/Public/js/yxj.js
@@ -59,22 +59,49 @@ function getScrollLeft(c) {
 function scolse() {  document.getElementById("yxj001").style.display = 'none';}
 function scolse2() {  document.getElementById("yxj002").style.display = 'none';}
 function scolse3() {  document.getElementById("yxj003").style.display = 'none';}
+var adtop1= 460;
+var adtop2= 460;
 function rightBottomAd(){
 		
-  document.getElementById("yxj001").style.top=(getScrollTop()+getClientHeight()-460)+"px";
+  document.getElementById("yxj001").style.top=(getScrollTop()+getClientHeight()-adtop1)+"px";
   document.getElementById("yxj001").style.left=(getClientWidth()-120)+"px";
   setTimeout(function(){rightBottomAd();},1);
 }
 function leftBottomAd(){
-  document.getElementById("yxj002").style.top=getScrollTop()+getClientHeight()-460+"px";
+  document.getElementById("yxj002").style.top=getScrollTop()+getClientHeight()-adtop2+"px";
   document.getElementById("yxj002").style.left=0+"px";
   setTimeout(function(){leftBottomAd();},1);
 }
 function youxiaAd(){
-  document.getElementById("yxj003").style.top=getScrollTop()+getClientHeight()-300+"px";
+  document.getElementById("yxj003").style.top=getScrollTop()+getClientHeight()-yxjtoppx+"px";
   document.getElementById("yxj003").style.left=(getClientWidth()-336)+"px";
   setTimeout(function(){youxiaAd();},1);
+}
+var moveflag1 = 0;
+var moveflag2 = 0;
+var moveflag3 = 0;
+var yxjtoppx = 300;
+function moveyxjdiv(){
+	document.getElementById("gbyxj3").onmouseover=function(){
+		if(moveflag3 == 0){
+			moveflag3 = 1;
+			setTimeout(function(){yxjtoppx = 330;},350);
+		}
+	}
+	document.getElementById("gbyxj2").onmouseover=function(){
+		if(moveflag2 == 0){
+			moveflag2 = 1;
+			setTimeout(function(){adtop2 = 500;},350);
+		}
+	}
+	document.getElementById("gbyxj1").onmouseover=function(){
+		if(moveflag1 == 0){
+			moveflag1 = 1;
+			setTimeout(function(){adtop1 = 500;},350);
+		}
+	}
 }
 rightBottomAd();
 leftBottomAd();
 youxiaAd();
+moveyxjdiv();
